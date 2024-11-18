488f8dddba44f093aff9dbe063765bf88c3006bc
Alexey Zauzin <lehadnk@gmail.com>
2024-11-18 20:31:45 +0300

    Removed https

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import * as fs from "node:fs";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,11 +8,5 @@ export default defineConfig({
   ],
   optimizeDeps: {
     include: ['msw'],
-  },
-  server: {
-    https: {
-      key: fs.readFileSync('./inf/tm.local+2-key.pem'),
-      cert: fs.readFileSync('./inf/tm.local+2.pem'),
-    },
   },
 })
