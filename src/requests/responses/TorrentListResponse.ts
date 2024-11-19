d3ca8db4703cec2632ee0276a432b39c06cbb504
y.zauzina <y.zauzina@tinkoff.ru>
2024-11-19 20:14:49 +0000

    add torrent list

--- src/requests/responses/TorrentListResponse.ts
+++ src/requests/responses/TorrentListResponse.ts
@@ -1,11 +1,32 @@
 export class Torrent {
+    id: number;
+    status: string;
 
+    constructor(id: number, status: string) {
+        this.id = id;
+        this.status = status;
+    }
 }
 
-export class TranmissionTorrent {
+export class TransmissionTorrent {
+    id: number;
+    name: string;
+    eta: string;
 
+    constructor(id: number, name: string, eta: string) {
+        this.id = id;
+        this.name = name;
+        this.eta = eta;
+    }
 }
 
 export class TorrentListResponse {
+    count: number;
+    torrents: Torrent[];
+
+    constructor(count: number) {
+        this.count = count;
+        this.torrents = [];
+    }
 
 }
